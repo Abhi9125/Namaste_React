e9f2eb2b9786d0c194d17aba8e984aceffecf193
Abhi9125 <abhi.singh3132@gmail.com>
2023-10-31 01:48:20 +0530

    part - 5 update and complete

--- Codes/app.js
+++ Codes/app.js
@@ -17,11 +17,11 @@ Creating the nested element using react
 
 //-- createElement take three argument first is `tag`, `Attribute`, `input that we want to publish`
 // if we have nested element and also sibling in it so we use `[]` in third argument
+// we can also do this type
+const heading1 = React.createElement("h1", {}, "I'm heading 1");
+const heading2 = React.createElement("h2", {}, "I'm heading 2");
 const nested = React.createElement("div", { parent }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm heading 1"),
-    React.createElement("h2", {}, "I'm heading 2"),
-  ]),
+  React.createElement("div", { id: "child1" }, [heading1, heading2]),
 
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm heading 1"),

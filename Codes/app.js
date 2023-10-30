35e88d51fbfb762c24eb781e4ab202c1545b2fdc
Abhi9125 <abhi.singh3132@gmail.com>
2023-10-30 22:27:52 +0530

    part - 4 completed

--- Codes/app.js
+++ Codes/app.js
@@ -2,23 +2,32 @@
 Creating the nested element using react 
 
 <div id = "parent">
-  <div id = "child">
-  <h1>I'm heading 1</h1>
-  <h2>I'm heading 2</h2>
+  <div id = "child1">
+    <h1>I'm heading 1</h1>
+    <h2>I'm heading 2</h2>
+  
+  <div id = "child2">
+    <h1>I'm heading 1</h1>
+    <h2>I'm heading 2</h2>
+</div>
+
 </div>
 
 */
 
 //-- createElement take three argument first is `tag`, `Attribute`, `input that we want to publish`
 // if we have nested element and also sibling in it so we use `[]` in third argument
-const nested = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 heading"),
-    React.createElement("h2", {}, "I'm h2 heading"),
-  ])
-);
+const nested = React.createElement("div", { parent }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm heading 1"),
+    React.createElement("h2", {}, "I'm heading 2"),
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm heading 1"),
+    React.createElement("h2", {}, "I'm heading 2"),
+  ]),
+]);
 
 console.log(nested); // object that have `h1` element and some props
 

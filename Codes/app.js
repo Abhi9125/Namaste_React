a01c6f74ab093e0b671048462dd64b5d06cb67c6
Abhi9125 <abhi.singh3132@gmail.com>
2023-11-04 04:16:07 +0530

    part - 3 completed,write code in JSX.

--- Codes/app.js
+++ Codes/app.js
@@ -9,6 +9,21 @@ const heading = React.createElement(
   "Hello react"
 );
 
+// JSX -- Jsx is combnation of html and JS but it not html in js.
+// jsx --convert> reactElement --convert> HtmlElement.
+// babel --> babel is coverter that conver JSX in JS.
+// react and JSX both are diffrent thing.
+// In JSX we use camelCase to give attribute.
+// In JSX we write inside `()` it. if code is multiple line so u must
+// write inside `()` but if cde is single line we do not nessary to
+// use `()`.
+// JSX Syntax
+const jsxHeading = (
+  <div className="Jsxheading">
+    <h1>This is JSX heading</h1>
+    <h2>If we want to write mutile line we must use `()`</h2>
+  </div>
+);
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(heading);
+root.render(jsxHeading);

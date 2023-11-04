f63465d888036af9e07bbf84794164cb882d6a9e
Abhi9125 <abhi.singh3132@gmail.com>
2023-11-04 20:07:40 +0530

    Part -5 completed

--- Codes/app.js
+++ Codes/app.js
@@ -1,64 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX -- Jsx is combnation of html and JS but it not html in js.
-// jsx --convert> reactElement --convert> HtmlElement.
-// babel --> babel is coverter that conver JSX in JS.
-// react and JSX both are diffrent thing.
-// In JSX we use camelCase to give attribute.
-// In JSX we write inside `()` it. if code is multiple line so u must
-// write inside `()` but if cde is single line we do not nessary to
-// use `()`.
+// Learning --
+// 1.how to use js code inside reactElement and component
+// 2. ReactElement composition
+// 3. Different way of composition of react component
+
 // JSX Syntax
 // --In last jsxHeading is reactElement
 const jsxHeading = (
   <div className="Jsxheading">
-    <h1>This is JSX heading</h1>
-    <h2>If we want to write mutile line we must use `()`</h2>
+    <h2>This is JSX heading</h2>
+    <h3>If we want to write mutile line we must use `()`</h3>
   </div>
 );
 
-// -- creating react functionanl component => it is a js function that return JSX.
-// we must write First capital letter in component
-const HeadingComponent = () => {
-  return <h1>I am a react functional component.</h1>;
-};
-
-// -- we can also write above line ike below
-// const HeadingComponent = () => <h1>I am a react functional component.</h1>
-
-// -- another functional component
-const DivComponent = () => {
+// By using `{}` we can run any js code.
+// -- use reactElement is side another reactElement
+const compJsx = (
+  <div className="Parent-Heading">
+    <h1>We composite react element inside another react element.</h1>
+    {jsxHeading}
+    {500 + 200}
+  </div>
+);
+// -- compose reactElement inside components
+const HeadingComp = function () {
   return (
     <div>
-      <h1>I am h1 heading inside div.</h1>
-      <h2>I am h2 heading inside div.</h2>
+      <h1>Composing react element inside component.</h1>
+      {compJsx}
     </div>
   );
 };
 
-// -- Converting above reactElement into ReactComponent
-const JsxHeading = () => (
-  <div className="Jsxheading">
-    <h1>This is JSX heading</h1>
-    <h2>If we want to write mutile line we must use `()`</h2>
-  </div>
-);
-
-// -- Component composition --> when we write a component inside a conponet is know as component composion.
-const CompComposition = () => {
+// -- Compose function Component by different type
+// -- We can compose react component by using below ways.
+const CompElements = () => {
   return (
     <div>
-      <HeadingComponent />
-      <h1>Call a component inside a component.</h1>
+      <p>I am composing recat functional component in different way.</p>
+      {HeadingComp()};
+      <HeadingComp />
+      <HeadingComp></HeadingComp>
     </div>
   );
 };
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-
-// We must write react component inside `<>`
-
-// root.render(<HeadingComponent />);
-// root.render(<DivComponent />);
-// root.render(<JsxHeading />);
-root.render(<CompComposition />);
+// root.render(compJsx);
+// root.render(<HeadingComp></HeadingComp>);
+root.render(<CompElements />);

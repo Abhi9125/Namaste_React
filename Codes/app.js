5fed4154b1c67609ac7bd2c60a2341df3748d812
Abhi9125 <abhi.singh3132@gmail.com>
2023-11-11 20:27:40 +0530

    Chapter - 4, part - 1 completed.

--- Codes/app.js
+++ Codes/app.js
@@ -1,52 +1,103 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Learning --
-// 1.how to use js code inside reactElement and component
-// 2. ReactElement composition
-// 3. Different way of composition of react component
+/*Learning in this episode 4, part - 1
+- make header component 
+- make body componet 
+- make resturant cart 
+- how to give inline styling in react
+*/
 
-// JSX Syntax
-// --In last jsxHeading is reactElement
-const jsxHeading = (
-  <div className="Jsxheading">
-    <h2>This is JSX heading</h2>
-    <h3>If we want to write mutile line we must use `()`</h3>
-  </div>
-);
+/*
+- Header
+  - Logo
+  - nav bar
+- Body
+ - search 
+ - resturent container 
+   - resturant carts 
+     - Img 
+     - Name of res, Star Rating, cuisines, etc.
+- Footer 
+  - Copyright 
+  - Links 
+  - Address
+  - contact
+*/
 
-// By using `{}` we can run any js code.
-// -- use reactElement is side another reactElement
-const compJsx = (
-  <div className="Parent-Heading">
-    <h1>We composite react element inside another react element.</h1>
-    {jsxHeading}
-    {500 + 200}
-  </div>
-);
-// -- compose reactElement inside components
-const HeadingComp = function () {
+// Hearder Component --  that hold the logo and nav bars
+const HeaderCom = () => {
+  return (
+    <header className="header">
+      <div className="img-container">
+        <img
+          className="image"
+          src="https://images-platform.99static.com/O3ZHfJeHB741xpyYH95tWvMsdTI=/0x0:1279x1279/500x500/top/smart/99designs-contests-attachments/63/63966/attachment_63966256"
+        />
+      </div>
+
+      <div className="nav-container">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Offers</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+// Reasturant Cart Component that hold information about the reaturent
+const RescartCom = () => {
   return (
     <div>
-      <h1>Composing react element inside component.</h1>
-      {compJsx}
+      <div className="res-cart-container">
+        <img
+          className="res-img"
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/u3ubwcgga6rr9jdxxobx"
+        />
+        <h3 className="name">Burger King</h3>
+        <h4 className="cuisines">Burger,American</h4>
+        <h4>4.2</h4>
+        <h4>38 mintues</h4>
+      </div>
     </div>
   );
 };
 
-// -- Compose function Component by different type
-// -- We can compose react component by using below ways.
-const CompElements = () => {
+// Body Component that hold search and reasturent cart.
+const BodyCom = () => {
   return (
-    <div>
-      <p>I am composing recat functional component in different way.</p>
-      {HeadingComp()};
-      <HeadingComp />
-      <HeadingComp></HeadingComp>
+    <div className="main-body-container">
+      <div className="search-container">Search</div>
+      <div className="res-container">
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+        <RescartCom />
+      </div>
     </div>
   );
 };
+
+// App component
+const AppContainer = () => {
+  return (
+    <div>
+      {" "}
+      <HeaderCom />
+      <BodyCom />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-// root.render(compJsx);
-// root.render(<HeadingComp></HeadingComp>);
-root.render(<CompElements />);
+root.render(<AppContainer />);
